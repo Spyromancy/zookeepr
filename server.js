a997f39823c0aa82f2ccbfd4fd584ed3452abc2a
Josh <josh.haddix598@gmail.com>
2022-08-02 13:25:08 -0500

    added front end files

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended:true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 
@@ -110,6 +111,16 @@ app.post('/api/animals', (req,res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+
+
+
 
 app.listen(PORT,() => {
     console.log(`API server now on port ${PORT}!`);

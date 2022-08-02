fc07ee1f6aff98cebdb7d3b0f80dd7e0625c0b81
Josh <josh.haddix598@gmail.com>
2022-08-02 13:51:21 -0500

    completed basic front-end integration

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function validateAnimal(animal) {
     if(!animal.diet || typeof animal.diet!== 'string'){
         return false;
     }
-    if(!animal.personalityTraits || typeof animal.personalityTraits!== 'string'){
+    if(!animal.personalityTraits ||  !Array.isArray(animal.personalityTraits)){
         return false;
     }
     return true;
@@ -116,10 +116,17 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
 
+app.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
 
-
-
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 
 app.listen(PORT,() => {
